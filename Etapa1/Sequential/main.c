104a6fd18d000b5d79d6d98dc91e5d07a4ef9ae3
Victor-Miron Boiangiu <miron.boiangiu@gmail.com>
2024-12-20 05:15:30 +0200

    feat: reorganize folder structure
    Signed-off-by: Victor-Miron Boiangiu <miron.boiangiu@gmail.com>

---
+++ Etapa1/Sequential/main.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdint.h>
+#include <string.h>
+
+#include "image_editor.h"
+
+int main(int argc, char *argv[]) {
+
+    int blur_count = 1;
+
+    if (argc < 3) {
+        fprintf(stderr, "Usage: %s <input file> <output file>\n", argv[0]);
+        return 1;
+    }
+
+    if (argc >= 4) {
+        blur_count = atoi(argv[3]);
+    }
+
+    IMAGE img;
+    open_bmp_image(argv[1], &img);
+
+    for (int i = 0; i < blur_count; i++) {
+        apply_gaussian_blur(&img);
+    }
+    
+    save_bmp_image(argv[1], argv[2], &img);
+    free_image(&img);
+
+    return 0;
+}
